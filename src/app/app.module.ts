fadf158633911cccaa41b0849c600a79eaf820f6
Adan Muktar <adosamjeshi@gmail.com>
2020-04-14 23:34:05 +0300

    added readme.md

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+import { FormsModule } from '@angular/forms';
+
+import { ProfileService } from './service/service.component';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
+import { StrikethroughDirective } from './strikethrough.directive';
+import { ServicesCountPipe } from './services-count.pipe';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProfileComponent,
+    StrikethroughDirective,
+    ServicesCountPipe,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [ProfileService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
